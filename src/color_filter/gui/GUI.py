459c86a2b6397e30b349941b654ecbeb3611bb45
Aitor Martinez <aitor.martinez.fernandez@gmail.com>
2017-04-17 09:11:41 +0200

    Prepared color_filter to pyqt4

--- src/color_filter/gui/GUI.py
+++ src/color_filter/gui/GUI.py
@@ -26,6 +26,7 @@ from gui.cameraWidget import CameraWidget
 from gui.communicator import Communicator
 from gui.sensorsWidget import SensorsWidget
 from gui.colorFilterWidget import  ColorFilterWidget
+from gui.logoWidget import LogoWidget
 
 class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
     
@@ -36,6 +37,10 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         self.teleop=TeleopWidget(self)
         self.tlLayout.addWidget(self.teleop)
         self.teleop.setVisible(True)
+
+        self.logo = LogoWidget(self, self.logoLayout.parent().width(), self.logoLayout.parent().height())
+        self.logoLayout.addWidget(self.logo)
+        self.logo.setVisible(True)
 
         self.record = False
 
